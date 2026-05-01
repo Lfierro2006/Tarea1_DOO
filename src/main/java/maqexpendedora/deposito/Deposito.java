9db2a326968de8b618ef536e0b236d6e90a43b41
kalotres <jmolina2024@inf.udec.cl>
2026-05-01 15:15:02 -0400

    chore: se movieron todos los productos a un package llamado producto refactor: ahora el deposito es generico, se puede usar para crear deposito de monedas, dulces,etc en el main

--- src/main/java/maqexpendedora/deposito/Deposito.java
+++ src/main/java/maqexpendedora/deposito/Deposito.java
@@ -1,20 +1,30 @@
 package deposito;
-public class Deposito{
-    private ArrayList<Bebida> lista;
+
+public class Deposito<T>{ // clase generica
+    private ArrayList<T> lista; //se define para que solo maneje del tipo T
     public Deposito(){
-        this.lista= new ArrayList<Bebida>();
-    }
-    public void addBebida(Bebida b){
-        lista.add(b);
-    }
-    public Bebida getBebida() {
-        if (lista.size() == 0) {
+
+        this.lista= new ArrayList<T>();
+    } //cuando se hace el arraylist le pasamos el tipo T
+    public void addObjeto(T objeto){ //añade el objeto y se comprueba primero que sea del mismo tipo T
+        lista.add(objeto);
+        }
+
+    public T getObjeto() { // extrae el objeto si hay disponible o regresa null si no
+        if (lista.isEmpty()) {
             return null;
-        } else{
-            return lista.remove(0);
+        }else{
+            return  lista.remove(0);
         }
     }
 }
+
+
+
+
+
+
+
 /* Deposito de monedas para unirlo al deposito normal
 class DepositoM {
     private ArrayList<Moneda> mlist;
