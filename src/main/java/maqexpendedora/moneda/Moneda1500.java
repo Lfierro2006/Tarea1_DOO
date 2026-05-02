343b282dfcecaa16fb0ec789cfba81e137fc19b1
Lfierro2006 <lfierro2025@inf.udec.cl>
2026-05-02 00:37:39 -0400

    Crear nueva Moneda1500

---
+++ src/main/java/maqexpendedora/moneda/Moneda1500.java
@@ -0,0 +1,11 @@
+package moneda;
+public class Moneda1500 extends Moneda{
+    public Moneda1500(){
+        super();
+    }
+
+    @Override
+    public int getValor() {
+        return 1500;
+    }
+}
