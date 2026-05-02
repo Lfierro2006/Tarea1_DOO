12bebfce94221196e5eeaeda08d3c34cd3f5a1aa
kalotres <jmolina2024@inf.udec.cl>
2026-05-01 22:30:25 -0400

    Nueva funcion general que unifica dulces y bebidas

---
+++ src/main/java/maqexpendedora/producto/Producto.java
@@ -0,0 +1,16 @@
+package producto;
+
+
+public abstract class Producto {
+    private int serie;
+
+    public Producto (int serie){
+        this.serie = serie;
+    }
+
+    public int getSerie() {
+        return serie;
+    }
+
+    public abstract String consumir();
+}
