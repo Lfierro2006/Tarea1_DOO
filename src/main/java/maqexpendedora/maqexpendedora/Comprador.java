dbaeb63c0fb4a03214918a4179e40a29318d618e
Lfierro2006 <lfierro2025@inf.udec.cl>
2026-05-01 23:33:00 -0400

    Adaptar Comprador a el nuevo Expendedor

--- src/main/java/maqexpendedora/maqexpendedora/Comprador.java
+++ src/main/java/maqexpendedora/maqexpendedora/Comprador.java
@@ -2,37 +2,27 @@ package maqexpendedora;
 
 import moneda.*;
 import producto.*;
-
-import static maqexpendedora.Comprador.CualProducto.FANTA;
-import static maqexpendedora.Comprador.TipoProducto.*;
-
 public class Comprador{
     private String sonido;
     private int vuelto;
-    enum TipoProducto{ BEBIDA, DULCE}
-    enum CualProducto{ SNICKER, SUPER8, TERCERDULCE, COCACOLA, FANTA, SPRITE}
-    public Comprador(Moneda m, TipoProducto tipoProducto,CualProducto cualProducto ,Expendedor exp){
-        switch (tipoProducto){
-
-        }
+    public Comprador(Moneda m, Expendedor.nomProduct product, Expendedor exp){
+        Producto p= exp.comprarProducto(m,product);
         if(p != null){
-            this.sonido=b.beber();
+            this.sonido=p.consumir();
         } else {
             this.sonido=null;
         }
-
         this.vuelto=0;
         Moneda monedavuelto = exp.getVuelto();
         while(monedavuelto != null){
             this.vuelto=this.vuelto+ monedavuelto.getValor();
             monedavuelto= exp.getVuelto();
-
         }
     }
     public int cuantoVuelto(){
         return this.vuelto;
     }
-    public String queBebiste(){
+    public String queConsumiste(){
         return  this.sonido;
     }
 }
